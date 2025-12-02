811d38cd961d77a68f4dcdb819142041d3e075df
kashikaryash <kashikaryash@gmail.com>
2025-12-02 10:27:01 +0530

    first commit

---
+++ IncidentManagementSystem/src/main/java/com/config/JacksonConverterConfig.java
@@ -0,0 +1,24 @@
+package com.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class JacksonConverterConfig implements WebMvcConfigurer {
+
+    @Bean
+    MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter(new ObjectMapper()));
+    }
+}
